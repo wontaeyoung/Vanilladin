7ff5afe9292ee69a3de072f1526ac669a93b3e0b
Taeyoung Won <hexter1994@gmail.com>
2023-07-25 03:01:23 +0900

    [Build Up] 스토리보드 삭제 -> 코드베이스 Scene 세팅

--- Clean-MVVM-UIKit-Vanilla-Store/Source/Application/AppDelegate.swift
+++ Clean-MVVM-UIKit-Vanilla-Store/Source/Application/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  Clean-MVVM-UIKit-Vanilla-Store
-//
-//  Created by 원태영 on 2023/07/25.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -18,7 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -30,7 +20,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
---
+++ Clean-MVVM-UIKit-Vanilla-Store/Source/Presenter/UI/View/InitialViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
