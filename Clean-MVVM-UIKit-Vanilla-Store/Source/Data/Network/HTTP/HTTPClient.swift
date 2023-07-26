7fe3a21734d05282247ebb8eb58db311211dbe5b
Taeyoung Won <hexter1994@gmail.com>
2023-07-27 03:20:39 +0900

    [Fix] Http Request 과정에서 에러 발생 시 로그를 출력하도록 수정

--- Clean-MVVM-UIKit-Vanilla-Store/Source/Data/Network/HTTP/HTTPClient.swift
+++ Clean-MVVM-UIKit-Vanilla-Store/Source/Data/Network/HTTP/HTTPClient.swift
@@ -9,6 +9,7 @@ final class HTTPClient: HTTPClientInterface {
         let components: URLComponents = getURLComponents(with: apiRequest)
         
         guard let url = components.url else {
+            print(#function, AladinAPIError.invalidURL.errorDescription)
             throw AladinAPIError.invalidURL
         }
         
@@ -17,14 +18,17 @@ final class HTTPClient: HTTPClientInterface {
         let (data, response): (Data, URLResponse) = try await URLSession.shared.data(for: request)
         
         guard let response = response as? HTTPURLResponse else {
+            print(#function, AladinAPIError.invalidResponse.errorDescription)
             throw AladinAPIError.invalidResponse
         }
         
         guard 200...299 ~= response.statusCode else {
+            print(#function, AladinAPIError.unexpectedStatusCode.errorDescription)
             throw AladinAPIError.unexpectedStatusCode
         }
         
         guard let decodedDTO = try? JSONDecoder().decode(decodeType, from: data) else {
+            print(#function, AladinAPIError.failToDecoding.errorDescription)
             throw AladinAPIError.failToDecoding
         }
         
@@ -39,6 +43,7 @@ private extension HTTPClient {
         components.host = apiRequest.host.rawValue
         components.path = apiRequest.endpoint.path
         components.queryItems = apiRequest.endpoint.queryItems
+        
         return components
     }
 }
