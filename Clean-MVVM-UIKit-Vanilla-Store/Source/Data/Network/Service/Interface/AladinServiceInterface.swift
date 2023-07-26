e13429b551319bddeca532798e55c12c62930e61
Taeyoung Won <hexter1994@gmail.com>
2023-07-27 01:31:33 +0900

    [Add] AladinServiceInterface 프로토콜 함수 requestCoverImage 정의

--- Clean-MVVM-UIKit-Vanilla-Store/Source/Data/Network/Service/Interface/AladinServiceInterface.swift
+++ Clean-MVVM-UIKit-Vanilla-Store/Source/Data/Network/Service/Interface/AladinServiceInterface.swift
@@ -1,6 +1,12 @@
+import UIKit
+
 protocol AladinServiceInterface {
     func requestBooks(
         query: String,
         page: Int
     ) async throws -> [BookDTO]
+    
+    func requestCoverImage(
+        _ urlStr: String
+    ) async throws -> UIImage
 }
