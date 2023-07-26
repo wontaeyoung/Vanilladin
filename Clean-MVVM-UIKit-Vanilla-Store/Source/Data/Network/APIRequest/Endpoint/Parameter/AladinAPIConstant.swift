6037fa57fff209c7fb897f37d0e595188b91011f
Taeyoung Won <hexter1994@gmail.com>
2023-07-27 04:00:36 +0900

    [Add] AladinAPI 고정 파라미터 값을 정리한 전역상수 추가

---
+++ Clean-MVVM-UIKit-Vanilla-Store/Source/Data/Network/APIRequest/Endpoint/Parameter/AladinAPIConstant.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum AladinAPIConstant {
+    static let ttbKey: String? = ProcessInfo.processInfo.environment["TTB_KEY"]
+    static let queryType: String = "Title"
+    static let cover: String = "Big"
+    static let output: String = "JS"
+    static let version: String = "20131101"
+}
