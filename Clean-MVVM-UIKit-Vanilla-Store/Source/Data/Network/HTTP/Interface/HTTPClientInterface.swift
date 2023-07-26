602059f03c81cdc0640f7fe2bdd1ca31fd9ff2c6
Taeyoung Won <hexter1994@gmail.com>
2023-07-27 04:41:27 +0900

    [Add] sendImageRequest 프로토콜 함수 추가

--- Clean-MVVM-UIKit-Vanilla-Store/Source/Data/Network/HTTP/Interface/HTTPClientInterface.swift
+++ Clean-MVVM-UIKit-Vanilla-Store/Source/Data/Network/HTTP/Interface/HTTPClientInterface.swift
@@ -1,6 +1,12 @@
+import UIKit
+
 protocol HTTPClientInterface {
-    func sendRequest<DTO: Decodable>(
+    func sendDTORequest<DTO: Decodable>(
         apiRequest: APIRequest,
         decodeType: DTO.Type
     ) async throws -> DTO
+    
+    func sendImageRequest(
+        urlStr: String
+    ) async throws -> UIImage?
 }
