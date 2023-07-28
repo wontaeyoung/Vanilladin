1f5c2479f06e773b3339108d3d0b9e85310413a8
Taeyoung Won <hexter1994@gmail.com>
2023-07-29 02:56:56 +0900

    [Feat] BookRepository 추가, URL로 커버 이미지를 받아와서 Entity로 매핑하는 fetchBook 구현

---
+++ MVVM-C_UIKit_Vanilla_Store/Source/Data/Repository/BookRepository.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+final class BookRepository {
+    // MARK: - Stored Property
+    private let aladinService: AladinService
+    
+    // MARK: - Initializer
+    init(
+        aladinService: AladinService = .init()
+    ) {
+        self.aladinService = aladinService
+    }
+    
+    // MARK: - Method
+    func fetchBook(
+        keyword: String,
+        page: UInt
+    ) async throws -> [Book] {
+        let booksDTO: [BookDTO] = try await aladinService.requestBooks(
+            keyword: keyword,
+            page: page
+        )
+        
+        let books: [Book] = try await AsyncManager.shared.mapConcurrently(from: booksDTO) { dto in
+            let coverImage: UIImage = try await self.aladinService.requestCoverImage(dto.cover)
+            return dto.asModel(with: coverImage)
+        }
+        
+        return books
+    }
+}
