7a884bc6e9af7d4d499da0586a7e6767209077d6
Taeyoung Won <hexter1994@gmail.com>
2023-08-01 04:17:28 +0900

    [Chore] resolve 설명 코멘트 마크다운 수정

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Container/DependencyContainerProtocol.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Container/DependencyContainerProtocol.swift
@@ -3,7 +3,9 @@ protocol DependencyContainerProtocol: DependencyContainable {
         instance: T
     )
     
-    // register에 원하는 인스턴스가 존재하지 않는 경우, 클로저로 전달받은 인스턴스를 등록 후 리턴할 수 있음
+    /**
+     register에 원하는 인스턴스가 존재하지 않는 경우, factory에 전달한 클로저로 인스턴스를 등록 후 리턴할 수 있습니다.
+     */
     func resolve<T: DependencyContainable>(
         _ factory: @escaping () -> T
     ) -> T
