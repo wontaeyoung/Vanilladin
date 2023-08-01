426282f14c2b665de54c71611f8dcf42a57a887b
Taeyoung Won <hexter1994@gmail.com>
2023-08-01 18:21:26 +0900

    [Add] InitialViewcontroller -> BookListViewController로 변경, 프로퍼티 및 이니셜라이저 설정

---
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/BookListViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+final class BookListViewController: BaseViewController {
+
+    // MARK: - Stored Property
+    private let bookListViewModel: BookListViewModel
+    
+    // MARK: - Initializer
+    init(
+        bookListViewModel: BookListViewModel
+    ) {
+        self.bookListViewModel = bookListViewModel
+        
+        super.init()
+    }
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        
+    }
+
+    
+    // MARK: - Method
+    
+}
