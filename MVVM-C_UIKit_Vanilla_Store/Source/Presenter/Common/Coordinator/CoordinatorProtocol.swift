e9d50501ac85437d238815df82e2dad0da5b5030
Taeyoung Won <hexter1994@gmail.com>
2023-08-06 09:00:57 +0900

    [Feat] CoordinatorProtocol에 handle 프로토콜 함수 추가, extension에 handle 로직 구현

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Coordinator/CoordinatorProtocol.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Coordinator/CoordinatorProtocol.swift
@@ -20,6 +20,7 @@ protocol CoordinatorProtocol: AnyObject {
     func pop()
     func dismissModal()
     func emptyOut()
+    func handle(error: AppErrorProtocol)
 }
 
 extension CoordinatorProtocol {
@@ -44,5 +45,27 @@ extension CoordinatorProtocol {
     
     func emptyOut() {
         navigationController.viewControllers.removeAll()
+    }
+    
+    func handle(error: AppErrorProtocol) {
+        
+        let alertController: UIAlertController = .init(
+            title: "오류 발생",
+            message: error.errorDescription,
+            preferredStyle: .alert
+        )
+        
+        let okAction: UIAlertAction = .init(
+            title: "확인",
+            style: .default
+        )
+        
+        alertController.addAction(okAction)
+        
+        self.navigationController
+            .present(
+                alertController,
+                animated: true
+            )
     }
 }
