951ff42d4efe1b37fad4c873611dd1caed7047b4
Taeyoung Won <hexter1994@gmail.com>
2023-08-08 17:31:33 +0900

    [Fix] 코드라인 수정, coordinator 파라미터 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Container/DependencyContainerProtocol.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Container/DependencyContainerProtocol.swift
@@ -1,12 +1,10 @@
 protocol DependencyContainerProtocol: DependencyContainable {
-    func register<T: DependencyContainable>(
-        instance: T
-    )
+    func register<T: DependencyContainable>(instance: T)
     
     /**
      register에 원하는 인스턴스가 존재하지 않는 경우, factory에 전달한 클로저로 인스턴스를 등록 후 리턴할 수 있습니다.
      */
     func resolve<T: DependencyContainable>() throws -> T
     
-    func setDependency()
+    func setDependency(coordinator: any CoordinatorProtocol)
 }
