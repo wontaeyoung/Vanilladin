cad5297a826bb81223feda957819c8f6772a2a72
Taeyoung Won <hexter1994@gmail.com>
2023-08-10 06:59:36 +0900

    [Feat] UITableView의 register와 dequeue 호출부를 간소화한 register, dequeueCell 함수 구현

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Base/BaseTableView.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Base/BaseTableView.swift
@@ -3,7 +3,26 @@ import UIKit
 class BaseTableView: UITableView {
     
     // MARK: - Method
-    func register() { }
+    func registerCells() { }
+    
+    func register(_ cellClass: UITableViewCell.Type) {
+        let identifier: String = .init(describing: cellClass)
+        
+        self.register(cellClass.self, forCellReuseIdentifier: identifier)
+    }
+    
+    func dequeueCell(
+        _ cellClass: UITableViewCell.Type,
+        for indexPath: IndexPath
+    ) -> UITableViewCell? {
+        let identifier: String = .init(describing: cellClass)
+        let cell: UITableViewCell? = self.dequeueReusableCell(
+            withIdentifier: identifier,
+            for: indexPath
+        )
+        
+        return cell
+    }
     
     // MARK: - Initializer
     init() {
@@ -12,11 +31,11 @@ class BaseTableView: UITableView {
             style: .plain
         )
         
-        self.register()
+        self.registerCells()
     }
     
     @available(*, unavailable)
     required init(coder: NSCoder) {
-        fatalError("NSCoding initializer")
+        fatalError("init(coder:) has not been implemented in BaseTableView")
     }
 }
