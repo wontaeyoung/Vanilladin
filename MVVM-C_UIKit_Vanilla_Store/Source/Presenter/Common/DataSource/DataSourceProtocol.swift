ef395c3933a0ddb22e2da1a49e1dca5db4a73f56
Taeyoung Won <hexter1994@gmail.com>
2023-08-10 07:42:08 +0900

    [Feat] entities 배열에 접근해서 단일 객체를 획득하는 entity 함수 구현

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/DataSource/DataSourceProtocol.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/DataSource/DataSourceProtocol.swift
@@ -2,4 +2,16 @@ protocol DataSourceProtocol: DependencyContainable {
     associatedtype Entity = EntityProtocol
     
     var entities: [Entity] { get set }
+    
+    func entity(at index: Int) -> Entity?
+}
+
+extension DataSourceProtocol {
+    func entity(at index: Int) -> Entity? {
+        guard index < entities.count else {
+            return nil
+        }
+        
+        return entities[index]
+    }
 }
