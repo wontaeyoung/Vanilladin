e3bc68994ea2475da8635974ba872c0b715eb496
Taeyoung Won <hexter1994@gmail.com>
2023-08-10 07:01:43 +0900

    [Feat] UIView의 오토레이아웃을 일괄적으로 적용하는 extension 함수 구현

--- MVVM-C_UIKit_Vanilla_Store/Source/Utility/Extension/UIView+.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Utility/Extension/UIView+.swift
@@ -12,4 +12,25 @@ extension UIView {
             view.translatesAutoresizingMaskIntoConstraints = false
         }
     }
+    
+    func setAutoLayoutAllEqual(to view: UIView) {
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: view.topAnchor),
+            self.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            self.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    func setPaddingAutoLayout(
+        to view: UIView,
+        padding: CGFloat
+    ) {
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            self.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            self.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            self.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+        ])
+    }
 }
