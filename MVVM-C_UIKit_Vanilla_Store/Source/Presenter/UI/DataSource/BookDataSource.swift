5c10a728c667b416d840ffa120672a8f540dbf04
Taeyoung Won <hexter1994@gmail.com>
2023-08-10 07:42:37 +0900

    [Feat] TableViewDataSource에 필요한 프로토콜 메서드 구현

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/DataSource/BookDataSource.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/DataSource/BookDataSource.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class BookDataSource: DataSourceProtocol {
+final class BookDataSource: NSObject, DataSourceProtocol {
     typealias Entity = Book
     
     // MARK: - Property
@@ -30,5 +30,27 @@ final class BookDataSource: DataSourceProtocol {
 }
 
 extension BookDataSource: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return entities.count
+    }
     
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let bookCell: BookCell = tableView.dequeueCell(BookCell.self, for: indexPath) as? BookCell else {
+            return tableView.dequeueCell(BookCell.self, for: indexPath)
+        }
+
+        guard let book: Book = entity(at: indexPath.row) else {
+            return bookCell
+        }
+        
+        bookCell.book = book
+        
+        return bookCell
+    }
 }
