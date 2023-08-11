56799759b591693fe06361b705fddabea0fabe69
Taeyoung Won <hexter1994@gmail.com>
2023-08-12 01:46:18 +0900

    [Feat] UserDefault를 통해 검색어 히스토리를 관리하고 조작하는 SearchKeywordHistoryDataSource 추가

---
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/DataSource/SearchKeywordHistoryDataSource.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+final class SearchKeywordHistoryDataSource: NSObject {
+    enum KeywordState {
+        case containKeyword
+        case greaterThanOrEqualMaxCount
+        case lessThanMaxCount
+    }
+    
+    // MARK: - Stored Property
+    let userDefault: UserDefaults
+    
+    // MARK: - Computed Property
+    private var keywords: [String] {
+        guard let keywords: [String] = userDefault.stringArray(
+            forKey: LogicConstant.UserDefault.searchKeywordKey
+        ) else {
+            return []
+        }
+        
+        return keywords
+    }
+    
+    // MARK: - Initializer
+    override init() {
+        self.userDefault = .standard
+    }
+    
+    // MARK: - Method
+    private func setKeywords(_ keywords: [String]) {
+        userDefault.set(keywords, forKey: LogicConstant.UserDefault.searchKeywordKey)
+    }
+    
+    private func keywordIndex(_ keyword: String) -> Int? {
+        return keywords.firstIndex(of: keyword)
+    }
+    
+    private func checkKeywordState(_ keyword: String) -> KeywordState {
+        if keywords.contains(keyword) {
+            return .containKeyword
+        }
+        
+        if keywords.count >= LogicConstant.UserDefault.maxKeywordCount {
+            return .greaterThanOrEqualMaxCount
+        }
+        
+        return .lessThanMaxCount
+    }
+    
+    private func insertKeywordAtFirst(_ keyword: String, for keywords: inout [String]) {
+        keywords.insert(keyword, at: 0)
+    }
+    
+    func saveKeyword(keyword: String) {
+        var keywords: [String] = self.keywords
+        
+        switch checkKeywordState(keyword) {
+            case .containKeyword:
+                if let index: Int = keywordIndex(keyword) { keywords.remove(at: index) }
+            case .greaterThanOrEqualMaxCount: keywords.removeLast()
+            case .lessThanMaxCount: break
+        }
+        
+        insertKeywordAtFirst(keyword, for: &keywords)
+        
+        setKeywords(keywords)
+    }
+}
