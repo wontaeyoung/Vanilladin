c06338fe7dc2a74c8ba9ec1d5d24deba82606ba2
Taeyoung Won <hexter1994@gmail.com>
2023-08-22 21:49:11 +0900

    [Add] SearchHistoryDataSource의 메서드들을 ViewController와 연결해주는 인터페이스 메서드 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/ViewModel/SearchHistoryViewModel.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/ViewModel/SearchHistoryViewModel.swift
@@ -14,4 +14,16 @@ final class SearchHistoryViewModel: ViewModelProtocol {
     func setTableViewDataSource(to tableView: BaseTableView) {
         dataSource.setTableViewDataSourceAsSelf(to: tableView)
     }
+    
+    func saveKeyword(_ keyword: String) {
+        dataSource.saveKeyword(keyword: keyword)
+    }
+    
+    func removeKeyword(_ index: Int) {
+        dataSource.removeKeyword(at: index)
+    }
+    
+    func removeAllKeywords() {
+        dataSource.removeAllKeywords()
+    }
 }
