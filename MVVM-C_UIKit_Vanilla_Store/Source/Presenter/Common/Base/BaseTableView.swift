986bb89fd186d89be08801c16dd6442258676c4c
Taeyoung Won <hexter1994@gmail.com>
2023-09-07 18:00:57 +0900

    [Chore] 코드라인 인덴트 2로 조정

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Base/BaseTableView.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Base/BaseTableView.swift
@@ -1,28 +1,27 @@
 import UIKit
 
 class BaseTableView: UITableView {
+  
+  // MARK: - Method
+  func registerCells() { }
+  func register(_ cellClass: UITableViewCell.Type) {
+    let identifier: String = .init(describing: cellClass)
     
-    // MARK: - Method
-    func registerCells() { }
+    self.register(cellClass.self, forCellReuseIdentifier: identifier)
+  }
+  
+  // MARK: - Initializer
+  init() {
+    super.init(
+      frame: .zero,
+      style: .plain
+    )
     
-    func register(_ cellClass: UITableViewCell.Type) {
-        let identifier: String = .init(describing: cellClass)
-        
-        self.register(cellClass.self, forCellReuseIdentifier: identifier)
-    }
-    
-    // MARK: - Initializer
-    init() {
-        super.init(
-            frame: .zero,
-            style: .plain
-        )
-        
-        self.registerCells()
-    }
-    
-    @available(*, unavailable)
-    required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented in BaseTableView")
-    }
+    self.registerCells()
+  }
+  
+  @available(*, unavailable)
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) BaseTableView")
+  }
 }
--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Base/BaseViewController.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Base/BaseViewController.swift
@@ -1,32 +1,32 @@
 import UIKit
 
 class BaseViewController: UIViewController, DependencyContainable {
+  
+  // MARK: - Method
+  func setAttribute() { }
+  func setHierarchy() { }
+  func setConstraint() { }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    // MARK: - Method
-    func setAttribute() { }
-    func setHierarchy() { }
-    func setConstraint() { }
+    self.view.backgroundColor = .white
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.view.backgroundColor = .white
-        
-        setAttribute()
-        setHierarchy()
-        setConstraint()
-    }
-    
-    // MARK: - Initializer
-    init() {
-        super.init(
-            nibName: nil,
-            bundle: nil
-        )
-    }
-    
-    @available(*, unavailable)
-    required init(coder: NSCoder) {
-        fatalError("NSCoding initializer")
-    }
+    setAttribute()
+    setHierarchy()
+    setConstraint()
+  }
+  
+  // MARK: - Initializer
+  init() {
+    super.init(
+      nibName: nil,
+      bundle: nil
+    )
+  }
+  
+  @available(*, unavailable)
+  required init(coder: NSCoder) {
+    fatalError("NSCoding initializer")
+  }
 }
