7d41c317d36e40daff99dc2f00cb49e68c90b289
Taeyoung Won <hexter1994@gmail.com>
2023-09-07 17:51:08 +0900

    [Add] BaseCollectionView 클래스 추가

---
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/Base/BaseCollectionView.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class BaseCollectionView: UICollectionView {
+  
+  // MARK: - Method
+  func registerCells() { }
+  func register(_ cellClass: UICollectionViewCell.Type) {
+    let identifier: String = .init(describing: cellClass)
+    
+    self.register(cellClass.self, forCellWithReuseIdentifier: identifier)
+  }
+  
+  // MARK: - Initializer
+  init(layout: UICollectionViewFlowLayout) {
+    super.init(frame: .zero, collectionViewLayout: layout)
+    
+    self.registerCells()
+  }
+  
+  @available(*, unavailable)
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) BaseCollectionView")
+  }
+}
