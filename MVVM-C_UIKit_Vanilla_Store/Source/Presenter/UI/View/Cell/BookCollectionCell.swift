ca026843052e87fafa3ab13fa0c03e13663a8a4e
Taeyoung Won <hexter1994@gmail.com>
2023-09-07 19:45:59 +0900

    [Add] BookCollectionCell 추가

---
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/Cell/BookCollectionCell.swift
@@ -0,0 +1,109 @@
+import UIKit
+
+final class BookCollectionCell: BaseCollectionViewCell {
+  // MARK: - Property
+  var book: Book? {
+    didSet {
+      self.setAttribute()
+    }
+  }
+  
+  // MARK: - UI
+  private let paddingView: UIView = .init()
+  
+  private let bookImageView: UIImageView = {
+    let imageView: UIImageView = .init()
+    imageView.contentMode = .scaleAspectFit
+    imageView.clipsToBounds = true
+    
+    return imageView
+  }()
+  
+  private let titleLabel: UILabel = {
+    let label: UILabel = .init()
+    label.font = .boldSystemFont(ofSize: UIConstant.FontSize.title)
+    label.numberOfLines = 0
+    
+    return label
+  }()
+  
+  private let authorLabel: UILabel = {
+    let label: UILabel = .init()
+    label.numberOfLines = 0
+    label.font = .systemFont(ofSize: UIConstant.FontSize.description)
+    
+    return label
+  }()
+  
+  private let publisherLabel: UILabel = {
+    let label: UILabel = .init()
+    label.numberOfLines = 0
+    label.font = .systemFont(ofSize: UIConstant.FontSize.description)
+    
+    return label
+  }()
+  
+  private let priceLabel: UILabel = {
+    let label: UILabel = .init()
+    label.numberOfLines = 0
+    label.font = .systemFont(ofSize: UIConstant.FontSize.description)
+    
+    return label
+  }()
+  
+  private lazy var infoStackView: UIStackView = {
+    let stackView: UIStackView = .init(arrangedSubviews: [
+      titleLabel,
+      authorLabel,
+      publisherLabel,
+      priceLabel
+    ])
+    
+    stackView.axis = .vertical
+    stackView.alignment = .leading
+    stackView.distribution = .fillProportionally
+    
+    return stackView
+  }()
+  
+  // MARK: - Initializer
+  
+  // MARK: - Method
+  override func setAttribute() {
+    bookImageView.image = book?.coverImage
+    titleLabel.attributedText = book?.title.attributedTitleText
+    authorLabel.text = book?.author
+    publisherLabel.text = book?.publisher
+    priceLabel.text = (book?.priceSales.asPriceString ?? "-") + "원"
+  }
+  
+  override func setHierarchy() {
+    contentView.addSubview(paddingView)
+    paddingView.addSubviews(bookImageView, infoStackView)
+  }
+  
+  override func setConstraint() {
+    setTranslatesAutoresizingMaskIntoConstraintsOff(
+      paddingView,
+      bookImageView,
+      infoStackView
+    )
+    
+    paddingView.setPaddingAutoLayout(to: contentView, padding: 10)
+//    paddingView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+    
+    NSLayoutConstraint.activate([
+      bookImageView.leadingAnchor.constraint(equalTo: paddingView.leadingAnchor),
+      bookImageView.topAnchor.constraint(equalTo: paddingView.topAnchor),
+      bookImageView.trailingAnchor.constraint(equalTo: paddingView.trailingAnchor),
+//      bookImageView.widthAnchor.constraint(equalTo: paddingView.heightAnchor, multiplier: 0.25),
+    ])
+    
+    NSLayoutConstraint.activate([
+      infoStackView.leadingAnchor.constraint(equalTo: paddingView.leadingAnchor),
+      infoStackView.trailingAnchor.constraint(equalTo: paddingView.trailingAnchor),
+      infoStackView.topAnchor.constraint(equalTo: bookImageView.topAnchor, constant: 10),
+      infoStackView.bottomAnchor.constraint(equalTo: paddingView.bottomAnchor),
+    ])
+  }
+}
