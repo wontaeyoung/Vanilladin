983da21330dd77b0fbe93154ac1d5b3fe91db0f1
Taeyoung Won <hexter1994@gmail.com>
2023-09-08 03:02:07 +0900

    [Add] DeviceSize 전역상수 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Utility/Constant/UIConstant.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Utility/Constant/UIConstant.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 enum UIConstant {
+    enum DeviceSize {
+        static let width: CGFloat = UIScreen.main.bounds.size.width
+        static let height: CGFloat = UIScreen.main.bounds.size.height
+    }
+    
     enum FontSize {
         static let title: CGFloat = 14.0
         static let description: CGFloat = 12.0
