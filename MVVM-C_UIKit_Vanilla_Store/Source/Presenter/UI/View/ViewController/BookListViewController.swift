37c8a11a8c19c66e82fa07f633495fb59653ea9d
Taeyoung Won <hexter1994@gmail.com>
2023-09-08 03:20:30 +0900

    [Style] BookCollectionView 추가, set UI, layout 속성 설정 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/ViewController/BookListViewController.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/ViewController/BookListViewController.swift
@@ -5,7 +5,8 @@ final class BookListViewController: BaseViewController {
     private let bookListViewModel: BookListViewModel
     
     // MARK: - UI
-    private(set) lazy var bookTableView: BookTableView = .init()
+    private lazy var bookTableView: BookTableView = .init()
+    private lazy var bookCollectionView: BookCollectionView = .init(layout: UICollectionViewFlowLayout())
     private var loadingIndicator: UIActivityIndicatorView = .init(style: .large)
     
     // MARK: - Initializer
@@ -19,16 +20,28 @@ final class BookListViewController: BaseViewController {
     override func setAttribute() {
         bookListViewModel.setDataSourceDelegate(self)
         bookListViewModel.setTableViewDataSource(to: bookTableView)
+        
+        guard let laytout = bookCollectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        
+        laytout.setAttribute(
+            direction: .vertical,
+            width: UIConstant.DeviceSize.width * 0.45,
+            height: UIConstant.DeviceSize.width,
+            lineSpacing: 20,
+            itemSpacing: 20,
+            inset: .init(top: 10, left: 10, bottom: 10, right: 10)
+        )
     }
     
     override func setHierarchy() {
-        view.addSubview(bookTableView)
+        view.addSubviews(bookTableView, bookCollectionView)
     }
     
     override func setConstraint() {
-        view.setTranslatesAutoresizingMaskIntoConstraintsOff(bookTableView)
+        view.setTranslatesAutoresizingMaskIntoConstraintsOff(bookTableView, bookCollectionView)
         
         bookTableView.setAutoLayoutAllEqual(to: view)
+        bookCollectionView.setAutoLayoutAllEqual(to: view)
     }
 }
 
