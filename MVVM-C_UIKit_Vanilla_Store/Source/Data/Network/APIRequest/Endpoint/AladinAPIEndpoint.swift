7fb8737b0da2f34670a5a8f7683db68acb3879f6
Taeyoung Won <hexter1994@gmail.com>
2023-09-07 17:57:52 +0900

    [Chore] case 인덴트 수정, 파라미터 줄바꿈 변경

--- MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/APIRequest/Endpoint/AladinAPIEndpoint.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/APIRequest/Endpoint/AladinAPIEndpoint.swift
@@ -1,53 +1,74 @@
 import Foundation
 
 enum AladinAPIEndpoint: EndpointProtocol {
-    case itemSearch(keyword: String, page: UInt)
-    case itemList
-    case itemLookUp
-    
-    var defaultPath: String {
-        return "/ttb/api"
+  case itemSearch(keyword: String, page: UInt)
+  case itemList
+  case itemLookUp
+  
+  var defaultPath: String {
+    return "/ttb/api"
+  }
+  
+  var optionalPath: String {
+    switch self {
+    case .itemSearch:
+      return "/ItemSearch.aspx"
+    case .itemList:
+      return "/ItemList.aspx"
+    case .itemLookUp:
+      return "/ItemLookUp.aspx"
     }
-    
-    var optionalPath: String {
-        switch self {
-            case .itemSearch:
-                return "/ItemSearch.aspx"
-            case .itemList:
-                return "/ItemList.aspx"
-            case .itemLookUp:
-                return "/ItemLookUp.aspx"
-        }
+  }
+  
+  var defaultQueryItems: [URLQueryItem] {
+    switch self {
+    case .itemSearch:
+      return [
+        URLQueryItem(
+          name: AladinAPIParameter.ttbKey.name,
+          value: AladinAPIConstant.ttbKey
+        ),
+        URLQueryItem(
+          name: AladinAPIParameter.QueryType.name,
+          value: AladinAPIConstant.queryType
+        ),
+        URLQueryItem(
+          name: AladinAPIParameter.Cover.name,
+          value: AladinAPIConstant.cover
+        ),
+        URLQueryItem(
+          name: AladinAPIParameter.Output.name,
+          value: AladinAPIConstant.output
+        ),
+        URLQueryItem(
+          name: AladinAPIParameter.Version.name,
+          value: AladinAPIConstant.version
+        ),
+      ]
+    case .itemList:
+      return []
+    case .itemLookUp:
+      return []
     }
-    
-    var defaultQueryItems: [URLQueryItem] {
-        switch self {
-            case .itemSearch:
-                return [
-                    URLQueryItem(name: AladinAPIParameter.ttbKey.name, value: AladinAPIConstant.ttbKey),
-                    URLQueryItem(name: AladinAPIParameter.QueryType.name, value: AladinAPIConstant.queryType),
-                    URLQueryItem(name: AladinAPIParameter.Cover.name, value: AladinAPIConstant.cover),
-                    URLQueryItem(name: AladinAPIParameter.Output.name, value: AladinAPIConstant.output),
-                    URLQueryItem(name: AladinAPIParameter.Version.name, value: AladinAPIConstant.version),
-                ]
-            case .itemList:
-                return []
-            case .itemLookUp:
-                return []
-        }
+  }
+  
+  var optionalQueryItems: [URLQueryItem] {
+    switch self {
+    case .itemSearch(let keyword, let page):
+      return [
+        URLQueryItem(
+          name: AladinAPIParameter.Query.name,
+          value: keyword
+        ),
+        URLQueryItem(
+          name: AladinAPIParameter.Start.name,
+          value: page.description
+        ),
+      ]
+    case .itemList:
+      return []
+    case .itemLookUp:
+      return []
     }
-    
-    var optionalQueryItems: [URLQueryItem] {
-        switch self {
-            case .itemSearch(let keyword, let page):
-                return [
-                    URLQueryItem(name: AladinAPIParameter.Query.name, value: keyword),
-                    URLQueryItem(name: AladinAPIParameter.Start.name, value: page.description),
-                ]
-            case .itemList:
-                return []
-            case .itemLookUp:
-                return []
-        }
-    }
+  }
 }
