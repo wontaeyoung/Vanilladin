9ab2c71b098ac6419906c60f4658867b111290be
Taeyoung Won <hexter1994@gmail.com>
2023-09-09 02:48:13 +0900

    [Chore] 상단 공백 삭제

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/Cell/BookTableCell.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/Cell/BookTableCell.swift
@@ -83,7 +83,6 @@ final class BookTableCell: BaseTableViewCell {
     }
     
     override func setConstraint() {
-        
         setTranslatesAutoresizingMaskIntoConstraintsOff(paddingView, bookImageView, infoStackView)
         
         paddingView.setPaddingAutoLayout(to: contentView, padding: 10)
