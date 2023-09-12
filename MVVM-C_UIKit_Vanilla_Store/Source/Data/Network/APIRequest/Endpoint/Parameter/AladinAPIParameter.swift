2ca47edcde127147bf65d7b9962546571806fb53
Taeyoung Won <hexter1994@gmail.com>
2023-09-13 08:16:42 +0900

    [Fix] 파라미터 열거형을 요청 케이스별로 분리, ItemLookUp 파라미터 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/APIRequest/Endpoint/Parameter/AladinAPIParameter.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/APIRequest/Endpoint/Parameter/AladinAPIParameter.swift
@@ -1,14 +1,33 @@
-enum AladinAPIParameter: String {
-    case ttbKey
-    case QueryType
-    case Cover
-    case Output
-    case Version
-    case Query
-    case Start
-    case MaxResults
+enum AladinAPIParameter {
+    enum Common: String {
+        case ttbKey
+        case version
+        case output
+        
+        var name: String {
+            return self.rawValue
+        }
+    }
     
-    var name: String {
-        return self.rawValue
+    enum ItemSearch: String {
+        case queryType
+        case cover
+        case query
+        case start
+        case maxResults
+        
+        var name: String {
+            return self.rawValue
+        }
+    }
+    
+    enum ItemLookUp: String {
+        case itemID
+        case itemIDType
+        case optResult
+        
+        var name: String {
+            return self.rawValue
+        }
     }
 }
