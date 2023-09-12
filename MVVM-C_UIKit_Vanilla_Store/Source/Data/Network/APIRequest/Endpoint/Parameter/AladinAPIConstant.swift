3453886b667b047a0dae1b9198680b1f0cb8fc22
Taeyoung Won <hexter1994@gmail.com>
2023-09-12 22:42:52 +0900

    [Fix] API 상수를 요청 케이스에 따라서 세부적으로 분리

--- MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/APIRequest/Endpoint/Parameter/AladinAPIConstant.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/APIRequest/Endpoint/Parameter/AladinAPIConstant.swift
@@ -1,10 +1,19 @@
 import Foundation
 
 enum AladinAPIConstant {
-    static let ttbKey: String? = ProcessInfo.processInfo.environment["TTB_KEY"]
-    static let queryType: String = "Title"
-    static let cover: String = "Big"
-    static let output: String = "JS"
-    static let version: String = "20131101"
-    static let maxResults: String = "10"
+    enum Common {
+        static let ttbKey: String? = ProcessInfo.processInfo.environment["TTB_KEY"]
+        static let output: String = "JS"
+        static let version: String = "20131101"
+    }
+    
+    enum ItemSearch {
+        static let queryType: String = "Title"
+        static let cover: String = "Big"
+        static let maxResults: String = "10"
+    }
+    
+    enum ItemLookUp {
+        static let itemIDType: String = "ISBN13"
+    }
 }
