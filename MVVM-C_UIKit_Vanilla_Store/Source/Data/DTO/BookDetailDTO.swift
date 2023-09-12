59d08cbf87d8e233d0b5802f281f2f9e4158f919
Taeyoung Won <hexter1994@gmail.com>
2023-09-13 06:30:34 +0900

    [Add] BookDetailDTO 추가, asModel 함수 구현

--- MVVM-C_UIKit_Vanilla_Store/Source/Data/DTO/BookDetailDTO.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Data/DTO/BookDetailDTO.swift
@@ -1,4 +1,29 @@
+import UIKit
+
+struct LookupBookResult: Codable {
+    let item: [BookDetailDTO]
+}
+
+struct BookDetailDTO: DTOProtocol {
+    let subInfo: SubInfo
+    
+    func asModel(with coverImage: UIImage?) -> BookDetail {
+        .init(
+            id: UUID(),
+            itemPage: subInfo.itemPage,
+            ratingScore: subInfo.ratingInfo.ratingScore,
+            ratingCount: subInfo.ratingInfo.ratingCount,
+            commentReviewCount: subInfo.ratingInfo.commentReviewCount,
+            myReviewCount: subInfo.ratingInfo.myReviewCount,
+            width: subInfo.packing.width,
+            height: subInfo.packing.height,
+            depth: subInfo.packing.depth,
+            weight: subInfo.packing.weight)
+    }
+}
+
 struct SubInfo: Codable {
+    let itemPage: Int
     let ratingInfo: RatingInfo
     let packing: PackingInfo
 }
