73aace6da2bf2bf6d4b448367ab91446b24b5edb
Taeyoung Won <hexter1994@gmail.com>
2023-09-12 23:59:14 +0900

    [Add] BookDetail Entity 추가

---
+++ MVVM-C_UIKit_Vanilla_Store/Source/Data/Entity/BookDetail.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct BookDetail: EntityProtocol {
+    let id: UUID
+    
+    let itemPage: Int
+    let ratingScore: Double
+    let ratingCount: Int
+    let commentReviewCount: Int
+    let myReviewCount: Int
+    let width: Int
+    let height: Int
+    let depth: Int
+    let weight: Int
+}
