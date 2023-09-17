2a5455fb512e340987fc4e0eaf90afc4b5604a1f
Taeyoung Won <hexter1994@gmail.com>
2023-09-18 08:48:41 +0900

    [Add] 프로퍼티 ratingScore , 이니셜라이저 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/RatingView.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/RatingView.swift
@@ -1,5 +1,14 @@
 import UIKit
 
 final class RatingView: BaseView {
+    // MARK: - Property
+    private let ratingScore: Int
+    
+    // MARK: - Initializer
+    init(ratingScore: Double) {
+        self.ratingScore = Int(ratingScore)
+        
+        super.init()
+    }
     
 }
