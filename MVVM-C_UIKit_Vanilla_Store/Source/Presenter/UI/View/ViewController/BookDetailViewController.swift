e1ac163641edda5569a8e445ed35cfe428424e15
Taeyoung Won <hexter1994@gmail.com>
2023-09-17 21:28:33 +0900

    [Add] 프로퍼티, 이니셜라이저 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/ViewController/BookDetailViewController.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/ViewController/BookDetailViewController.swift
@@ -2,7 +2,19 @@ import UIKit
 
 final class BookDetailViewController: BaseViewController {
     // MARK: - Property
+    private let bookDetail: BookDetail
+    private let bookDetailViewModel: BookDetailViewModel
     // MARK: - UI
     // MARK: - Initializer
+    init(
+        bookDetail: BookDetail,
+        bookDetailViewModel: BookDetailViewModel
+    ) {
+        self.bookDetail = bookDetail
+        self.bookDetailViewModel = bookDetailViewModel
+        
+        super.init()
+    }
+    
     // MARK: - Method
 }
