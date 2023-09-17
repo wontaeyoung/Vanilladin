d7591d74b3f70ac1f171adfcb71840ed4fa727c8
Taeyoung Won <hexter1994@gmail.com>
2023-09-17 21:18:47 +0900

    [Chore] 함수 로직에 대한 심플 코멘트 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/HTTP/HTTPClient.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/HTTP/HTTPClient.swift
@@ -2,6 +2,7 @@ import UIKit
 
 final class HTTPClient: DependencyContainable {
     // MARK: - Method
+    /// http 방식의 API로 데이터를 요청해서 응답을 전달받은 디코딩 타입으로 변환해서 반환하는 함수
     func sendDTORequest<DTO: Decodable>(
         apiRequest: APIRequest,
         decodeType: DTO.Type
@@ -23,6 +24,7 @@ final class HTTPClient: DependencyContainable {
         return decodedDTO
     }
     
+    /// 이미지 URL로 응답받은 이미지 데이터를 UIImage로 변환해서 반환하는 함수
     func sendImageRequest(urlStr: String) async throws -> UIImage? {
         guard let url = URL(string: urlStr) else {
             print(#function, AladinAPIError.invalidURL.errorDescription)
@@ -36,6 +38,7 @@ final class HTTPClient: DependencyContainable {
 }
 
 private extension HTTPClient {
+    /// input으로 들어온 apiRequest의 속성들을 통해서 scheme, host, query parameter가 설정된 URL Component를 만들어서 반환하는 함수
     func getURLComponents(with apiRequest: APIRequest) -> URLComponents {
         var components: URLComponents = .init()
         components.scheme = apiRequest.scheme.rawValue
@@ -46,6 +49,7 @@ private extension HTTPClient {
         return components
     }
     
+    /// URL Component로 완성된 URL을 통해 데이터를 요청해서 반환하는 함수
     func sendRequestWithURL(url: URL) async throws -> Data {
         let request: URLRequest = .init(url: url)
         let (data, response): (Data, URLResponse) = try await URLSession.shared.data(for: request)
