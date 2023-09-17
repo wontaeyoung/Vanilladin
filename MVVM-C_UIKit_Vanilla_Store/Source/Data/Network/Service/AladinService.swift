ba16a712a2bcbeab2fda81189459749b19f0a34c
Taeyoung Won <hexter1994@gmail.com>
2023-09-17 21:19:32 +0900

    [Chore] requestBooks 함수 코멘트 마크업 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/Service/AladinService.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Data/Network/Service/AladinService.swift
@@ -10,6 +10,15 @@ final class AladinService: DependencyContainable {
     }
     
     // MARK: - Method
+    /**
+     검색어로 API 요청을 수행해서 검색 결과에 해당하는 데이터를 반환합니다.
+     
+     - parameters:
+     - parameterkeyword: 검색어 query에 해당하는 문자열입니다.
+     - parameterpage: 검색어에 해당하는 결과 중 가져올 페이지 번호입니다. maxResults 파라미터에 따라 포함되는 결과가 변경됩니다.
+     
+     - Returns: 전체 결과 갯수, 책 정보 리스트를 포함한 구조체를 반환합니다.
+     */
     func requestBooks(keyword: String, page: UInt) async throws -> SearchBookResult {
         let apiRequest: APIRequest = .init(
             scheme: .https,
