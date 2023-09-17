40cccab42d17dc9ca8be16271fb635d3604a1675
Taeyoung Won <hexter1994@gmail.com>
2023-09-17 23:47:16 +0900

    [Add] Book 프로퍼티 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/ViewController/BookDetailViewController.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/ViewController/BookDetailViewController.swift
@@ -2,14 +2,18 @@ import UIKit
 
 final class BookDetailViewController: BaseViewController {
     // MARK: - Property
+    private let book: Book
     private let bookDetail: BookDetail
     private let bookDetailViewModel: BookDetailViewModel
+    
     // MARK: - UI
     // MARK: - Initializer
     init(
+        book: Book,
         bookDetail: BookDetail,
         bookDetailViewModel: BookDetailViewModel
     ) {
+        self.book = book
         self.bookDetail = bookDetail
         self.bookDetailViewModel = bookDetailViewModel
         
