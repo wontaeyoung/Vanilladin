a5157be5c52b2d387fbe1a2e37c790dc17c562fe
Taeyoung Won <hexter1994@gmail.com>
2023-09-17 23:47:58 +0900

    [Style] 책 이미지와 기본정보 UI 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/ViewController/BookDetailViewController.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/ViewController/BookDetailViewController.swift
@@ -7,6 +7,30 @@ final class BookDetailViewController: BaseViewController {
     private let bookDetailViewModel: BookDetailViewModel
     
     // MARK: - UI
+    private let bookImageView: UIImageView = {
+        let imageView: UIImageView = .init()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .boldSystemFont(ofSize: UIConstant.FontSize.title)
+        label.numberOfLines = .zero
+        
+        return label
+    }()
+    
+    private let authorPublisherLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: UIConstant.FontSize.description)
+        label.numberOfLines = .zero
+        
+        return label
+    }()
+    
     // MARK: - Initializer
     init(
         book: Book,
