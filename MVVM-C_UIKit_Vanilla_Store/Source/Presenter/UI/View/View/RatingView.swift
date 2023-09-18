b02833e8a0c73370e23d648271549b6167dee2de
Taeyoung Won <hexter1994@gmail.com>
2023-09-18 14:57:20 +0900

    [Feat] 리뷰 점수로 별점 이미지를 그려서 stack에 추가하는 로직 구현

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/RatingView.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/RatingView.swift
@@ -4,6 +4,14 @@ final class RatingView: BaseView {
     // MARK: - Property
     private let ratingScore: Int
     
+    // MARK: - UI
+    private let starStackView: UIStackView = {
+        let stackView: UIStackView = .init()
+        stackView.axis = .horizontal
+        
+        return stackView
+    }()
+    
     // MARK: - Initializer
     init(ratingScore: Double) {
         self.ratingScore = Int(ratingScore)
@@ -11,4 +19,28 @@ final class RatingView: BaseView {
         super.init()
     }
     
+    // MARK: - Method
+    private func setStars(ratingScore: Int) {
+        let fullStarCount: Int = ratingScore / 2
+        let hasHalfStar: Bool = ratingScore % 2 == 1
+        
+        for starNum in 1...UIConstant.Rating.maxStarRatingCount {
+            let imageView: UIImageView = {
+                let imageView: UIImageView = .init()
+                imageView.contentMode = .scaleAspectFit
+                
+                return imageView
+            }()
+            
+            let symbol: String = starNum <= fullStarCount
+            ? UIConstant.SFSymbol.starFill
+            : hasHalfStar
+            ? UIConstant.SFSymbol.starLeadinghalfFill
+            : UIConstant.SFSymbol.star
+            
+            imageView.image = UIImage(systemName: symbol)
+            
+            starStackView.addArrangedSubview(imageView)
+        }
+    }
 }
