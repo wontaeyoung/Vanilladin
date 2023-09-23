eafced08b9739aba8d2e9d4d62173e3184e6be6c
Taeyoung Won <hexter1994@gmail.com>
2023-09-24 03:09:37 +0900

    [Fix] Double의 소수점 올림 변환 시, 소수점 아래가 없어도 .0으로 표현하도록 수정

--- MVVM-C_UIKit_Vanilla_Store/Source/Utility/Extension/Double+.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Utility/Extension/Double+.swift
@@ -4,6 +4,7 @@ extension Double {
     func string(rounded place: Int) -> String {
         let nsValue: NSNumber = .init(value: self)
         let formatter: NumberFormatter = .init()
+        formatter.minimumFractionDigits = 1
         formatter.maximumFractionDigits = place
         
         guard let formattedValue = formatter.string(from: nsValue) else {
