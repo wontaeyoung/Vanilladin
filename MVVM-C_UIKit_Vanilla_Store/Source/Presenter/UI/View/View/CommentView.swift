cde290ff9846edaddcbc140efe9383e4699d1bc5
Taeyoung Won <hexter1994@gmail.com>
2023-09-24 23:59:47 +0900

    [Add] 프로퍼티 bookDetail 및 이니셜라이저, 코멘트 심볼 이미지와 카운트 라벨 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/CommentView.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/CommentView.swift
@@ -1,5 +1,32 @@
 import UIKit
 
 final class CommentView: BaseView {
+    // MARK: - Property
+    private let bookDetail: BookDetail
+    
+    // MARK: - UI
+    private let commentImage: UIImageView = {
+        let imageView: UIImageView = .init()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(systemName: UIConstant.SFSymbol.textBubble)?
+            .colored(with: .black)
+        
+        return imageView
+    }()
+    
+    private let commentCountLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: UIConstant.FontSize.description)
+        label.numberOfLines = .zero
+        
+        return label
+    }()
+    
+    // MARK: - Initializer
+    init(bookDetail: BookDetail) {
+        self.bookDetail = bookDetail
+        
+        super.init(frame: .zero)
+    }
     
 }
