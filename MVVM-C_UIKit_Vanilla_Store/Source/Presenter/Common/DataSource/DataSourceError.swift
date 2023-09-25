434897a2ce93710e58deaff5d2c0a3aa1ae57281
Taeyoung Won <hexter1994@gmail.com>
2023-09-25 22:15:41 +0900

    [Add] noISBN13 에러 케이스 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/DataSource/DataSourceError.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/Common/DataSource/DataSourceError.swift
@@ -1,10 +1,14 @@
 enum DataSourceError: AppErrorProtocol {
     case findEntityFailed(entityName: String)
+    case noISBN13
     
     var errorDescription: String {
         switch self {
         case let .findEntityFailed(entityName):
             return "\(entityName) 정보를 찾는데 실패했습니다. 다시 시도해주세요."
+            
+        case .noISBN13:
+            return "ISBN13을 지원하지 않는 도서입니다."
         }
     }
 }
