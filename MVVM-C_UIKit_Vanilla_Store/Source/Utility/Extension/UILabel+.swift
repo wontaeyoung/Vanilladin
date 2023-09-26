443a60518227c654369586410a939ba4fd13af1c
Taeyoung Won <hexter1994@gmail.com>
2023-09-26 23:53:14 +0900

    [Feat] UILabel의 렌더링 높이를 계산해서 1줄이 넘어가는지 체크하는 isTruncated 변수 추가

---
+++ MVVM-C_UIKit_Vanilla_Store/Source/Utility/Extension/UILabel+.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension UILabel {
+    var isTruncated: Bool {
+        guard let labelText = text else {
+            return false
+        }
+        
+        let labelTextSize = (labelText as NSString).boundingRect(
+            with: CGSize(width: frame.width, height: .greatestFiniteMagnitude),
+            options: .usesLineFragmentOrigin,
+            attributes: [NSAttributedString.Key.font: font ?? UIFont()],
+            context: nil).size
+        
+        return labelTextSize.height > bounds.size.height
+    }
+}
