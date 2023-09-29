80eaa37d27478e9af09cddc1e16999b669daca80
Taeyoung Won <hexter1994@gmail.com>
2023-09-29 16:55:32 +0900

    [Fix] 텍스트가 비어있는 경우에도 라벨 높이를 계산하는 오류 수정

--- MVVM-C_UIKit_Vanilla_Store/Source/Utility/Extension/UILabel+.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Utility/Extension/UILabel+.swift
@@ -2,15 +2,19 @@ import UIKit
 
 extension UILabel {
     var isTruncated: Bool {
-        guard let labelText = text else {
+        guard 
+            let labelText = text,
+            labelText.isEmpty == false
+        else {
             return false
         }
         
-        let labelTextSize = (labelText as NSString).boundingRect(
-            with: CGSize(width: frame.width, height: .greatestFiniteMagnitude),
-            options: .usesLineFragmentOrigin,
-            attributes: [NSAttributedString.Key.font: font ?? UIFont()],
-            context: nil).size
+        let labelTextSize = (labelText as NSString)
+            .boundingRect(
+                with: CGSize(width: frame.width, height: .greatestFiniteMagnitude),
+                options: .usesLineFragmentOrigin,
+                attributes: [NSAttributedString.Key.font: font ?? UIFont()],
+                context: nil).size
         
         return labelTextSize.height > bounds.size.height
     }
