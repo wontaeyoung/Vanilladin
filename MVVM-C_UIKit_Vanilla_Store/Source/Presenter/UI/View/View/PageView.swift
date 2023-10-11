369db88293c9387d4e043762e99ef98059cecb0b
Taeyoung Won <hexter1994@gmail.com>
2023-10-12 00:08:18 +0900

    [Style] pageView 뷰 계층구조, 제약사항 설정

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/PageView.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/PageView.swift
@@ -30,5 +30,28 @@ final class PageView: BaseView {
     }
     
     // MARK: - Method
+    override func setAttribute() {
+        pageValueLabel.text = page.description
+    }
     
+    override func setHierarchy() {
+        addSubviews(pageImageView, pageValueLabel)
+    }
+    
+    override func setConstraint() {
+        setTranslatesAutoresizingMaskIntoConstraintsOff(pageImageView, pageValueLabel)
+        
+        NSLayoutConstraint.activate([
+            pageImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            pageImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            pageImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            pageImageView.widthAnchor.constraint(equalToConstant: 25)
+        ])
+        
+        NSLayoutConstraint.activate([
+            pageValueLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            pageValueLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            pageValueLabel.leadingAnchor.constraint(equalTo: self.pageImageView.trailingAnchor, constant: 10)
+        ])
+    }
 }
