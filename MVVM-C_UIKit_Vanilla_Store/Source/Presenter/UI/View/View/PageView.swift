c9c865718cb5a9bec63047206caaad535d85eb01
Taeyoung Won <hexter1994@gmail.com>
2023-10-12 00:03:22 +0900

    [Add] 페이지 이미지 뷰, 페이지 쪽 수 라벨링 UI 프로퍼티 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/PageView.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/PageView.swift
@@ -5,10 +5,28 @@ final class PageView: BaseView {
     private let page: Int
     
     // MARK: - UI
+    private let pageImageView: UIImageView = {
+        let imageView: UIImageView = .init()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(systemName: UIConstant.SFSymbol.bookPages)?
+            .colored(with: .black)
+        
+        return imageView
+    }()
+    
+    private let pageValueLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: UIConstant.FontSize.title)
+        label.numberOfLines = .zero
+        
+        return label
+    }()
     
     // MARK: - Initializer
     init(page: Int) {
         self.page = page
+        
+        super.init(frame: .zero)
     }
     
     // MARK: - Method
