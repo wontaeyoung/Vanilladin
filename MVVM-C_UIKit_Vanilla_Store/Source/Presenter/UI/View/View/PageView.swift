1838661632045cc36a4bd20d9673cc39f5738759
Taeyoung Won <hexter1994@gmail.com>
2023-10-11 23:59:55 +0900

    [Add] 책 페이지 수를 표시하는 PageView 추가

---
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/PageView.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+final class PageView: BaseView {
+    // MARK: - Property
+    private let page: Int
+    
+    // MARK: - UI
+    
+    // MARK: - Initializer
+    init(page: Int) {
+        self.page = page
+    }
+    
+    // MARK: - Method
+    
+}
