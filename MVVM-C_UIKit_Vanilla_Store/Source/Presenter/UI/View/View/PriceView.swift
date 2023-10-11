0af4b4d76bf3025bbb7708d458d9b3540719e406
Taeyoung Won <hexter1994@gmail.com>
2023-10-11 23:40:34 +0900

    [Clean] priceLabel을 priceValueLabel로 변경

--- MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/PriceView.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Presenter/UI/View/View/PriceView.swift
@@ -14,7 +14,7 @@ final class PriceView: BaseView {
         return imageView
     }()
     
-    private let priceLabel: UILabel = {
+    private let priceValueLabel: UILabel = {
         let label: UILabel = .init()
         label.font = .boldSystemFont(ofSize: UIConstant.FontSize.title)
         label.numberOfLines = .zero
@@ -31,15 +31,15 @@ final class PriceView: BaseView {
     
     // MARK: - Method
     override func setAttribute() {
-        priceLabel.text = price.asPriceFormat
+        priceValueLabel.text = price.asPriceFormat
     }
     
     override func setHierarchy() {
-        addSubviews(priceImage, priceLabel)
+        addSubviews(priceImage, priceValueLabel)
     }
     
     override func setConstraint() {
-        setTranslatesAutoresizingMaskIntoConstraintsOff(priceImage, priceLabel)
+        setTranslatesAutoresizingMaskIntoConstraintsOff(priceImage, priceValueLabel)
         
         NSLayoutConstraint.activate([
             priceImage.topAnchor.constraint(equalTo: self.topAnchor),
@@ -49,9 +49,9 @@ final class PriceView: BaseView {
         ])
         
         NSLayoutConstraint.activate([
-            priceLabel.topAnchor.constraint(equalTo: self.topAnchor),
-            priceLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            priceLabel.leadingAnchor.constraint(equalTo: priceImage.trailingAnchor, constant: 10),
+            priceValueLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            priceValueLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            priceValueLabel.leadingAnchor.constraint(equalTo: priceImage.trailingAnchor, constant: 10),
         ])
     }
 }
