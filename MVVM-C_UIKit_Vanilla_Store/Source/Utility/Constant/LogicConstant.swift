21ccf68e356302ab6d266931f0fe6335813ab4de
Taeyoung Won <hexter1994@gmail.com>
2023-10-16 21:48:30 +0900

    [Add] Plist 관련 상수 추가

--- MVVM-C_UIKit_Vanilla_Store/Source/Utility/Constant/LogicConstant.swift
+++ MVVM-C_UIKit_Vanilla_Store/Source/Utility/Constant/LogicConstant.swift
@@ -1,4 +1,13 @@
 enum LogicConstant {
+    enum Plist {
+        static let apiKeyFileName: String = "API_KEY"
+        static let apiKey: String = "API_KEY"
+        
+        enum FileType {
+            static let plist: String = "plist"
+        }
+    }
+    
     enum UserDefault {
         static let searchKeywordKey: String = "SearchKeyword"
         static let maxKeywordCount: Int = 5
