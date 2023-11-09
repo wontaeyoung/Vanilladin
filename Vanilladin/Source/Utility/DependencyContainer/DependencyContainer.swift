54d56e3efbb0574b66903515c663858962d5c2b1
Taeyoung Won <hexter1994@gmail.com>
2023-11-09 20:04:24 +0900

    [Fix] setDependency 함수에 coordinator를 전달하지 않도록 수정
    타입 캐스팅으로 확인하지 않고 Coordinator에서 직접 할당하도록 변경함

--- Vanilladin/Source/Utility/DependencyContainer/DependencyContainer.swift
+++ Vanilladin/Source/Utility/DependencyContainer/DependencyContainer.swift
@@ -33,7 +33,7 @@ final class DependencyContainer: DependencyContainerProtocol {
      
      DependencyModules 열거형 케이스를 전달받아서 자동으로 추가하는 방법을 구현 시도했으나, 인스턴스 초기화 의존성을 추상화하지 못해서 폐기되었습니다.
      */
-    func setDependency(coordinator: CoordinatorProtocol) {
+    func setDependency() {
         // MARK: - Netowrk
         let httpClient: HTTPClient = .init()
         let aladinService: AladinService = .init(httpClient: httpClient)
--- Vanilladin/Source/Utility/DependencyContainer/DependencyContainerProtocol.swift
+++ Vanilladin/Source/Utility/DependencyContainer/DependencyContainerProtocol.swift
@@ -1,5 +1,5 @@
 protocol DependencyContainerProtocol: DependencyContainable {
     func register<T: DependencyContainable>(instance: T)
     func resolve<T: DependencyContainable>() throws -> T
-    func setDependency(coordinator: any CoordinatorProtocol)
+    func setDependency()
 }
