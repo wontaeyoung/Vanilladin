ecd1fa466bd7e3e3792b73d55b17655d24a2761f
Taeyoung Won <hexter1994@gmail.com>
2023-12-10 00:01:06 +0900

    [Feat] WritableEntityProtocol 채택, asDTO 상세로직 구현

--- Vanilladin/Source/Data/Entity/BookMemo.swift
+++ Vanilladin/Source/Data/Entity/BookMemo.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct BookMemo {
+struct BookMemo: WritableEntityProtocol {
     enum ReadState: String {
         case interested
         case reading
@@ -12,4 +12,20 @@ struct BookMemo {
     let isbn13: String
     let memo: String
     var state: ReadState
+    
+    static var dummy: Self {
+        .init(
+            id: UUID(),
+            isbn13: LogicConstant.ISBN13.favoriteBook,
+            memo: "",
+            state: .noState)
+    }
+    
+    func asDTO() -> BookMemoDTO {
+        .init(
+            id: id.uuidString,
+            isbn13: isbn13,
+            memo: memo,
+            state: state.rawValue)
+    }
 }
