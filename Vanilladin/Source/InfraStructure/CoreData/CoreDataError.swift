d21d4f45519607ea4155199c93dbbbd3cd0fea0d
Taeyoung Won <hexter1994@gmail.com>
2023-12-12 02:04:43 +0900

    [Feat] 코어데이터 에러에 대한 설명 문구 작성

--- Vanilladin/Source/InfraStructure/CoreData/CoreDataError.swift
+++ Vanilladin/Source/InfraStructure/CoreData/CoreDataError.swift
@@ -1,4 +1,14 @@
 enum CoreDataError: AppErrorProtocol {
     case storeConnectFailed
     case noResultWithQuery
+    
+    var errorDescription: String {
+        switch self {
+            case .storeConnectFailed:
+                return "저장소 연결에 실패했습니다."
+                
+            case .noResultWithQuery:
+                return "조건에 해당하는 결과를 찾을 수 없습니다."
+        }
+    }
 }
